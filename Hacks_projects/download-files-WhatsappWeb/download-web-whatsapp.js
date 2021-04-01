beb47f8e6cc90f58c2982a9dc3c10f4dd881cf69
Paulo Raoni <raoni_jkd@hotmail.com>
2021-03-31 22:05:20 -0300

    Adjusting files

--- Hacks_projects/download-files-WhatsappWeb/download-web-whatsapp.js
+++
@@ -1,9 +0,0 @@
-const body = document.querySelector('body');
-
-body.addEventListener("keyup", function(event) {
-  const download = document.querySelector(`[title="Download"]`);
-  if (event.keyCode === 40) {
-	event.preventDefault();
-	download.click();
-  }
-});
